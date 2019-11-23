631dace498426b43cafb9e7dd6281f0d2347db8b
KenSasamotoPrivate <sasamotoken@gmail.com>
2019-11-23 12:40:26 +0900

    DeleteImageService 親クラスを継承

--- service/DeleteImageService.php
+++ service/DeleteImageService.php
@@ -1,32 +1,13 @@
 <?php
-class DeleteImageService {
-    
-    private $pdo;
-    
-    public function __construct(){
+require_once('ImageServiceParent.php');
 
-        if($this->isLoggedIn() === false){
-            header('Location: http://' . $_SERVER['HTTP_HOST'].'/controller/LoginController.php');          
-            exit;
-        }
-
-        if(!isset($_SESSION['token'])) {
-            $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(16));
-        }
-
-        $this->pdo = new PDO(DSN, user, pass);
-
-    }
-    
+class DeleteImageService extends ImageServiceParent {
+        
     public function delete() {
         $sql = "DELETE FROM media WHERE id = :id";
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(":id", $_POST['id'], PDO::PARAM_INT);
         $stmt->execute();
-    }
-
-    protected function isLoggedIn(){
-        return isset($_SESSION['me']) && !empty($_SESSION['me']);
     }
 
 }
