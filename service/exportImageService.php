7379eeed7cc2dfcdf926c39cc5e0d13e441d0ab6
KenSasamotoPrivate <sasamotoken@gmail.com>
2019-11-23 10:47:05 +0900

    exportImageService 修正 else句無くす。GetImageを始めにnew

--- service/exportImageService.php
+++ service/exportImageService.php
@@ -2,19 +2,14 @@
 require_once('../config.php');
 require_once('GetImageService.php');
 
+$GetImageService = new GetImageService();
 
-if(isset($_GET["id"]) && $_GET["id"] !== ""){
-    $id = $_GET["id"];
-}
-else{
+if(!isset($_GET["id"]) || $_GET["id"] == ""){
     header('Location: http://' . $_SERVER['HTTP_HOST'].'/controller/IndexController.php');          
     exit;
 }
 
-$GetImageService = new GetImageService();
-
 $image = $GetImageService->findById($_GET['id']);
-
 
 header("Content-Type: ".$image->extension);
 echo ($image->raw_data);
