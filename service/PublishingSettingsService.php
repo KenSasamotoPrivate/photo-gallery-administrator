e9835c88ce33c02f514a78b6c785287724c25d07
KenSasamotoPrivate <sasamotoken@gmail.com>
2019-11-23 16:48:03 +0900

    PublishingSettingsService 親クラスを継承

--- service/PublishingSettingsService.php
+++ service/PublishingSettingsService.php
@@ -1,22 +1,7 @@
 <?php
-class PublishingSettingsService {
-    
-    private $pdo;
-    
-    public function __construct(){
+require_once('ImageServiceParent.php');
 
-        if($this->isLoggedIn() === false){
-            header('Location: http://' . $_SERVER['HTTP_HOST'].'/controller/LoginController.php');          
-            exit;
-        }
-
-        if(!isset($_SESSION['token'])) {
-            $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(16));
-        }
-
-        $this->pdo = new PDO(DSN, user, pass);
-
-    }
+class PublishingSettingsService extends ImageServiceParent {
 
     public function stateUpdate() {
 
@@ -52,14 +37,7 @@ class PublishingSettingsService {
 
         $this->pdo->commit();
 
-        return ['status' => $status];
-        
+        return ['status' => $status];    
     }
-
-    protected function isLoggedIn(){
-        return isset($_SESSION['me']) && !empty($_SESSION['me']);
-    }
-
 }
-
 ?>
