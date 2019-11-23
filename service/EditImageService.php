7ab7cd87dd7f42f9a1afdee57b9a8b6201acb1da
KenSasamotoPrivate <sasamotoken@gmail.com>
2019-11-23 12:26:05 +0900

    EditImageService 親クラスを継承

--- service/EditImageService.php
+++ service/EditImageService.php
@@ -1,27 +1,14 @@
 <?php
-//require_once('ImageService.php');
+require_once('ImageServiceParent.php');
 require_once('../model/UploadedData.php');
 
-class EditImageService {
+class EditImageService extends ImageServiceParent {
     
-    private $pdo;
     public $uploadedData;
 
     public function __construct(){
-
-        if($this->isLoggedIn() === false){
-            header('Location: http://' . $_SERVER['HTTP_HOST'].'/controller/LoginController.php');          
-            exit;
-        }
-
-        if(!isset($_SESSION['token'])) {
-            $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(16));
-        }
-
-        $this->pdo = new PDO(DSN, user, pass);
-
+        parent::__construct();
         $this->$uploadedData = new UploadedData;
-
     }
 
     public function editProcess(){
@@ -75,10 +62,6 @@ class EditImageService {
 
         header('Location: http://' . $_SERVER['HTTP_HOST'].'/view/edit_complete.php');
 
-    }
-
-    protected function isLoggedIn(){
-        return isset($_SESSION['me']) && !empty($_SESSION['me']);
     }
 
 }
